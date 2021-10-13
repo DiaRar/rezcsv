00d4d553f5928548db29f205aec1ae6947d385ef
pupiraresdi <34660424+pupiraresdi@users.noreply.github.com>
2021-10-13 18:51:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const app = require('./server');
+const app = require('./express/server');
 
 app.listen(3000, () => console.log(`Example app listening at http://192.168.0.60:3000`));
